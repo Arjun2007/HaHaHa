18ad6d86e1697dc183604eabe7aa5e045c3e9621
Arjun2007 <70872603+Arjun2007@users.noreply.github.com>
2020-09-07 09:34:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+var bubbles =[];
+
+function setup() {
+  createCanvas(600,400);
+  
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+  bubble3 = new Bubble();
+  bubble4 = new Bubble();
+  bubble5 = new Bubble();
+  bubble6 = new Bubble();
+  bubble7 = new Bubble();
+  bubble8 = new Bubble();
+  bubble9 = new Bubble();
+  bubble10 = new Bubble();
+  bubble11 = new Bubble();
+  bubble12= new Bubble();
+  bubble13= new Bubble();
+  bubble14 = new Bubble();
+  bubble15 = new Bubble();
+  bubble16 = new Bubble();
+  bubble17 = new Bubble();
+  bubble18 = new Bubble();
+  bubble19 = new Bubble();
+  bubble20 = new Bubble();
+  bubble21 = new Bubble();
+  bubble22 = new Bubble();
+  bubble23= new Bubble();
+  bubble24 = new Bubble();
+  bubble25 = new Bubble();
+  bubble26 = new Bubble();
+  bubble27 = new Bubble();
+  bubble28 = new Bubble();
+  bubble29 = new Bubble();
+  bubble30 = new Bubble();
+ 
+}
+
+function draw() {
+  background(0);
+  bubble1.move();
+  bubble2.move();
+  bubble3 .move();
+  bubble4 .move();
+  bubble5 .move();
+  bubble6 .move();
+  bubble7 .move();
+  bubble8 .move();
+  bubble10 .move();
+  bubble11 .move();
+  bubble12.move();
+  bubble13.move();
+  bubble14 .move();
+  bubble15 .move();
+  bubble16.move();
+  bubble17 .move();
+  bubble18 .move();
+  bubble19 .move();
+  bubble20 .move();
+  bubble21 .move();
+  bubble22.move();
+  bubble23.move();
+  bubble24 .move();
+  bubble25 .move();
+  bubble26 .move();
+  bubble27 .move();
+  bubble28 .move();
+  bubble29 .move();
+  bubble30 .move();
+
+ 
+  
+  bubble1.display();
+  bubble2.display();
+  bubble3.display();
+  bubble4 .display();
+  bubble5 .display();
+  bubble6 .display();
+  bubble7 .display();
+  bubble8 .display();
+  bubble10 .display();
+  bubble11 .display();
+  bubble12.display();
+  bubble13.display();
+  bubble14 .display();
+  bubble15 .display();
+  bubble16.display();
+  bubble17 .display();
+  bubble18 .display();
+  bubble19 .display();
+  bubble20 .display();
+  bubble21 .display();
+  bubble22.display();
+  bubble23.display();
+  bubble24 .display();
+  bubble25 .display();
+  bubble26 .display();
+  bubble27 .display();
+  bubble28.display();
+  bubble29 .display();
+  bubble30 .display();
+
+}
+
+    
